b15f1a3f2162ede1cbdd15a4e211482891756d1a
Administrator <Administrator@KMJ>
2018-05-01 11:41:07 +0900

    첫번째 업로드 180501

---
+++ 뇌를 자극하는 자바/src/FirstJava.java
@@ -0,0 +1,16 @@
+/**
+ * 2018. 4. 30.Dev by KIM.M.J
+   
+   FirstJava.java
+ */
+
+public class FirstJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		
+	}
+
+}
