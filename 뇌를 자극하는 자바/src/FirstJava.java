f5fd090e9463b71ac0f0c47ac92c40edfe810c6a
Administrator <Administrator@KMJ>
2018-05-11 11:18:17 +0900

    tnwjd

--- 뇌를 자극하는 자바/src/FirstJava.java
+++ 뇌를 자극하는 자바/src/FirstJava.java
@@ -8,9 +8,15 @@ public class FirstJava {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!");
+		int number1 = 30, number2 = 120;
+		int total = plus(number1, number2);
+		System.out.println("total:" + total);
 		
-		
+	}
+	
+	static int plus(int a, int b)
+	{
+		return a + b;
 	}
 
 }
