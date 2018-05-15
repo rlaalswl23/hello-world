2457deccd9634a49990e5b86a9a6abe938229dac
Administrator <Administrator@KMJ>
2018-05-15 09:12:15 +0900

    0515

---
+++ 뇌를 자극하는 자바/src/chap09/MathExample3.java
@@ -0,0 +1,17 @@
+/**
+ * 2018. 5. 15. Dev by KIM.M.J
+   chap09
+   MathExample3.java
+ */
+package chap09;
+
+public class MathExample3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+		System.out.println(Math.random());
+	}
+
+}
