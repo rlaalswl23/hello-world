fdf27e48ba89b97ff146c564c879276106f04425
Administrator <Administrator@KMJ>
2018-05-15 09:57:58 +0900

    3

--- 뇌를 자극하는 자바/src/chap09/MathExample3.java
+++ 뇌를 자극하는 자바/src/chap09/MathExample3.java
@@ -9,9 +9,15 @@ public class MathExample3 {
 
 	public static void main(String[] args) {
 		
-		System.out.println(Math.random());
-		System.out.println(Math.random());
-		System.out.println(Math.random());
+	for(int i = 1; i <= 7; i++) {	
+		
+		double randomValue = Math.random();
+		int value = (int)(randomValue * 45) + 1;
+	    System.out.println(i + "번째 난수 : " + value);
+	}
+//		System.out.println(Math.random());
+//		System.out.println(Math.random());
+//		System.out.println(Math.random());
 	}
 
 }
--- 뇌를 자극하는 자바/src/chap09/RandomExample2.java
+++ 뇌를 자극하는 자바/src/chap09/RandomExample2.java
@@ -1,16 +1,23 @@
 package chap09;
 
 import java.util.Random;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class RandomExample2 {
 
 	public static void main(String[] args) {
 
-		Random random = new Random(349239L);
+		Date date = new Date();
+		System.out.println(date.getTime());
 		
-		System.out.println(random.nextInt(100));
-		System.out.println(random.nextInt(100));
-		System.out.println(random.nextInt(100));
+		GregorianCalendar gcalendar = new GregorianCalendar();
+		System.out.println(gcalendar.getTimeInMillis());
+		
+		Random random = new Random(gcalendar.getTimeInMillis());
+		System.out.println(random.nextInt(100) + 1);
+		System.out.println(random.nextInt(100) + 1);
+		System.out.println(random.nextInt(100) + 1);
 		
 	}
 
